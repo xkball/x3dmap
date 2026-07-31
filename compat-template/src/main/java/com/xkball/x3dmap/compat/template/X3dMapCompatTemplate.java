a093c19f5153e552fe15ff644b21e9dda3e249b2
xkball <45330674+xkball@users.noreply.github.com>
2026-07-31 18:06:02 +0800

    compat template
    Took 11 minutes

---
+++ compat-template/src/main/java/com/xkball/x3dmap/compat/template/X3dMapCompatTemplate.java
@@ -0,0 +1,12 @@
+package com.xkball.x3dmap.compat.template;
+
+import com.xkball.x3dmap.X3dMap;
+import com.xkball.xklibmc.annotation.NonNullByDefault;
+import net.neoforged.fml.common.Mod;
+
+@Mod(X3dMapCompatTemplate.MODID)
+@NonNullByDefault
+public final class X3dMapCompatTemplate {
+
+    public static final String MODID = X3dMap.MODID + "_compat_template";
+}
